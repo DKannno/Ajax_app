ae8e4f95c67fd82dd3026d0e666b5d66846fcc5c
DKannno <kannno.0810@gmail.com>
2020-09-17 19:06:24 +0900

    既読機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,14 +15,15 @@ class PostsController < ApplicationController
 
   # checkedアクションは、「既読」の操作を行ったときに実行されるアクションです。
   def checked
+    # binding.pry
     post = Post.find(params[:id])
-    if post.checked 
+    if post.checked then
       post.update(checked: false)
     else
       post.update(checked: true)
     end
-
     item = Post.find(params[:id])
     render json: { post: item }
   end
+
 end
