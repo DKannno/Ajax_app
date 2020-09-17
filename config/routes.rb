9ecf37486fdd6ef6de582e8aabbdd71d0ee984a4
DKannno <kannno.0810@gmail.com>
2020-09-17 14:24:58 +0900

    既読機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root to: 'posts#index' 
   # get 'posts/new', to: 'posts#new' ←削除
-#  get'posts',to:'posts#index'
-#  get 'posts/new', to: 'posts#new'
- post 'posts', to: 'posts#create'
+  #  get'posts',to:'posts#index'
+  #  get 'posts/new', to: 'posts#new'
+  post 'posts', to: 'posts#create'
+  #  get 'posts', to: 'posts#checked'
+  # 既読機能のエンドポイントは、queryパラメーターで設定しました
+  # しかし、今回のように渡す情報が一意の情報であればpathパラメーターの方が適しています。
+  get 'posts/:id', to: 'posts#checked'
 end
+ 
